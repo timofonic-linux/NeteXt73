94a5889bf1e74ef675ec0c9a9998c057a71155d4
NetBit73 <netbit73@gmail.com>
2016-11-01 17:21:47 +0100

    czyszczenie kodu

--- czekaj.py
+++ czekaj.py
@@ -9,10 +9,10 @@ class ProgressBarWindow(Gtk.Window):
         sefl.set_border_width(5)
         sefl.set_position(Gtk.WindowPosition.CENTER)
 	sefl.set_icon_from_file('/opt/NeteXt73/menu/NeteXt73.png')
-        
+
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         sefl.add(vbox)
-	
+
         sefl.progressbar = Gtk.ProgressBar()
         vbox.pack_start(sefl.progressbar, True, True, 0)
 
