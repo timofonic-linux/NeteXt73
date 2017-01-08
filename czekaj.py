c188a2cf732f5e44b0cac56d651469d0265187f6
NetBit73 <netbit73@gmail.com>
2017-01-08 21:26:23 +0100

    poprawki pythona

--- czekaj.py
+++ czekaj.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
+import gi
+import os
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GObject
 
 class ProgressBarWindow(Gtk.Window):
 
     def __init__(sefl):
         Gtk.Window.__init__(sefl, title="NeteXt'73 - Please Wait")
-        sefl.set_default_size(450, 35)
+        sefl.set_default_size(450, 45)
         sefl.set_border_width(5)
         sefl.set_position(Gtk.WindowPosition.CENTER)
 	sefl.set_icon_from_file('/opt/NeteXt73/menu/NeteXt73.png')
@@ -20,11 +23,16 @@ class ProgressBarWindow(Gtk.Window):
 
     def on_timeout(sefl, user_data):
 	text = "Work in progress - please wait!"
+	sciezkaDoPliku="/tmp/netext73/zakoncz.txt"
+	if os.path.isfile(sciezkaDoPliku) :
+		os.unlink(sciezkaDoPliku)
+		exit()
 	sefl.progressbar.set_text(text)
 	sefl.progressbar.set_show_text(text)
         sefl.progressbar.pulse()
 	return True
-
+	
+		
 win = ProgressBarWindow()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
